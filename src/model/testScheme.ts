20547e258120ead5b4e6ac2aec3be98b929369c4
MuniMunii <tyuimiti@gmail.com>
2026-01-01 20:12:54 +0700

    refactor:Test endpoints and remodel scheme

--- src/model/testScheme.ts
+++ src/model/testScheme.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb'
 import {z}from 'zod'
 export const metaTestDataScheme=z.object({
     type:z.enum(['listening','reading','speaking','writing'],'type does not exist'),
@@ -8,4 +9,14 @@ export const metaTestDataScheme=z.object({
     time:z.enum(['30m','60m','120m','180m']),
     published:z.boolean()
 })
+export const questionScheme=z.object({
+    testId:z.instanceof(ObjectId),//Ref from meta test
+    order:z.number(),
+    qTitle:z.string(),
+    choices:z.array(z.object({
+        cTitle:z.string(),
+        correctAnswer:z.boolean()
+    })).min(2,'Must contain atleast 2 elements').max(5,'5 is max elements')
+})
 export type metaTestDataType=z.infer<typeof metaTestDataScheme>
+export type questionType=z.infer<typeof questionScheme>
--- src/routes/tests/tests.routes.ts
+++ src/routes/tests/tests.routes.ts
@@ -8,9 +8,10 @@ import { createResponse } from "../../utils/createResponse.js";
 import {
   metaTestDataScheme,
   type metaTestDataType,
+  type questionType,
 } from "../../model/testScheme.js";
 import { slugify } from "../../utils/slugify.js";
-import { initIndexes } from "../../utils/initIndex.js";
+import { ObjectId } from "mongodb";
 const router = Express.Router();
 router.post(
   "/create-test",
@@ -18,68 +19,62 @@ router.post(
   requireAuth,
   requireRoleAdmin("admin"),
   async (req: Request, res: Response) => {
+    const session = (await clientPromise).startSession();
     try {
-      const { type, title, description, isFree } = req.body;
-      console.log(type, title);
-      if (!type || !description || !title ||typeof isFree!=='boolean')
-        return res
-          .status(403)
-          .json(
-            createResponse(
-              false,
-              "All values must be inputed",
-              null,
-              "All values must be inputed"
-            )
-          );
-      const db = (await clientPromise).db("muniquizNew");
-      const collectionMetaTest = db.collection("meta-tests");
-      const data: metaTestDataType = {
-        title,
-        titleSlug: slugify(title),
-        type,
-        time: "120m",
-        description,
-        isFree,
+      const parsed = metaTestDataScheme.safeParse({
+        ...req.body,
+        titleSlug: slugify(req.body.title),
         published: false,
-      };
-      if (!metaTestDataScheme.safeParse(data).success)
-        return res.json(
-          createResponse(false, "Incorrect payload", null, "Incorrect payload")
-        );
-      const isTestAlreadyExist = await collectionMetaTest.findOne({
-        type: type,
-        title,
+        time: "120m",
       });
-      if (isTestAlreadyExist)
+      if (!parsed.success) {
         return res
-          .status(403)
-          .json(
-            createResponse(
-              false,
-              "Test already exist",
-              null,
-              "Test already exist"
-            )
-          );
-      await collectionMetaTest.insertOne(data);
-      res
+          .status(400)
+          .json(createResponse(false, "Invalid payload", null, parsed.error));
+      }
+      const db = (await clientPromise).db("muniquizNew");
+      const metaTests = db.collection("meta-tests");
+      const questions = db.collection("questions-test");
+      session.startTransaction();
+      const metaTestRes = await metaTests.insertOne(parsed.data, { session });
+      const questionData: questionType = {
+        testId: metaTestRes.insertedId,
+        order: 1,
+        qTitle: "Question Title",
+        choices: [
+          { cTitle: "Title choices 1", correctAnswer: true },
+          { cTitle: "Title choices 2", correctAnswer: false },
+        ],
+      };
+      await questions.insertOne(questionData, { session });
+      await session.commitTransaction();
+      return res
         .status(201)
-        .json(createResponse(true, "Successfully Created", data, false));
-    } catch (err) {
+        .json(createResponse(true, "Successfully Created", parsed.data, false));
+    } catch (err: any) {
+      await session.abortTransaction();
+      // Duplicate key error
+      if (err.code === 11000) {
+        return res
+          .status(409)
+          .json(createResponse(false, "Test already exists", null, err));
+      }
       return res
         .status(500)
         .json(createResponse(false, "Internal Server Error", null, err));
+    } finally {
+      await session.endSession();
     }
   }
 );
+
 router.post(
-  "/get-metadata-test",
+  "/get-metadata-test/:type/:titleSlug",
   sessionMiddleware,
   requireAuth,
   async (req: Request, res: Response) => {
     try {
-      const { type, titleSlug } = req.body;
+      const { type, titleSlug } = req.params;
       if (!type || !titleSlug)
         return res
           .status(403)
@@ -108,4 +103,98 @@ router.post(
     }
   }
 );
+router.post(
+  "/get-test/:type",
+  sessionMiddleware,
+  requireAuth,
+  async (req: Request, res: Response) => {
+    try {
+      const { type } = req.params;
+      const db = (await clientPromise).db("muniquizNew");
+      const metaTestCollections = db.collection("meta-tests");
+      const findTestByType = await metaTestCollections.find({ type }).toArray();
+      if (findTestByType.length === 0)
+        return res
+          .status(204)
+          .json(
+            createResponse(
+              false,
+              `Test ${type} by is empty`,
+              null,
+              "Test is empty"
+            )
+          );
+      res
+        .status(200)
+        .json(
+          createResponse(true, "Successfully fetch data", findTestByType, null)
+        );
+    } catch (err) {
+      return res
+        .status(500)
+        .json(createResponse(false, "Internal Server Error", null, err));
+    }
+  }
+);
+router.delete(
+  "/delete-test",
+  sessionMiddleware,
+  requireAuth,
+  requireRoleAdmin("admin"),
+  async (req: Request, res: Response) => {
+    const session = (await clientPromise).startSession();
+    try {
+      const { id } = req.body;
+      const db = (await clientPromise).db("muniquizNew");
+      const _id = ObjectId.createFromHexString(id);
+      const metaTestCollections = db.collection("meta-tests");
+      const questionCollections = db.collection("questions-test");
+      session.startTransaction();
+      const findMetaTest = await metaTestCollections.findOne(
+        { _id },
+        { session }
+      );
+      if (!findMetaTest)
+        return res
+          .status(404)
+          .json(
+            createResponse(false, "Test not found", null, "Test not found")
+          );
+      await Promise.all([
+        metaTestCollections.deleteOne({ _id }, { session }),
+        questionCollections.deleteMany({ testId: _id }, { session }),
+      ]);
+      await session.commitTransaction();
+      res
+        .status(200)
+        .json(createResponse(true, "Successfully deleted test", null, false));
+    } catch (err) {
+      await session.abortTransaction();
+      return res
+        .status(500)
+        .json(createResponse(false, "Internal Server Error", null, err));
+    } finally {
+      await session.endSession();
+    }
+  }
+);
+router.post('/get-question/admin/:testId',
+  sessionMiddleware,
+  requireAuth,
+  requireRoleAdmin("admin"),async (req:Request,res:Response)=>{
+    try{
+    const {testId}=req.params
+    if(!testId||!ObjectId.isValid(testId))return res.status(400).json(createResponse(false,'Incorrect testId',null,'Incorrect testId'))
+    const _id=ObjectId.createFromHexString(testId);
+    const questionCollections=(await clientPromise).db('muniquizNew').collection('questions-test')
+    const findQuestions=await questionCollections.find({testId:_id}).toArray()
+    if(findQuestions.length===0)return res.status(404).json(createResponse(false,"There are no question",null,'There are No question'))
+      res.status(200).json(createResponse(true,'Successfully fetch questions',findQuestions,null))
+    }
+    catch (err) {
+      return res
+        .status(500)
+        .json(createResponse(false, "Internal Server Error", null, err));
+    }
+  })
 export default router;
